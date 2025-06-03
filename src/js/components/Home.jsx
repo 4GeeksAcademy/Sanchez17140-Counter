d752d30dc5ea1777e80cbf1ad4f2a7143d8273cc
Sanchez17140 <sanchez17140@gmail.com>
2025-06-03 04:01:14 +0000

    First commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,36 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+//import container, { col, Row } from "./Grid";
+//import TimeClock from "./TimeClock";
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+ const Home = () => {
+    const[seconds,setSeconds]=useState(0);
+    useEffect(()=>{
+     const startCounter = setInterval(()=>{
+        setSeconds(previousSeconds=>previousSeconds+1)
+     console.log(seconds)
+    },1000)
+    return()=>clearInterval(startCounter)},[])
+    const formatTime = (time)=>{
+       return time.toString().padStart(6,"0").split("")
+    }
+    const digits=formatTime(seconds)
+    return (
+       <div className="d-flex justify-content-center">
+        <div className="text-center bg-dark" style={{height:"250px", width:"900px", margin:"200px"}}>
+            <div id="container" className= "d-flex flex-column align-items-center flex-md-row justify-content-between text-center">
+               {digits.map((digits,index)=>(
+                <div key={index} className="bg-dark" style={{height:"200px", width:"100px", margin:"20px"}}>
+                    <p style={{ fontSize: "110px", color: "white" }}>{digits}</p>
+                </div>
+               ))}
+            </div>
+        </div>
+       </div>
+    );
 };
-
-export default Home;
+export default Home
---
+++ src/js/components/Timeclock.jsx
@@ -0,0 +1,12 @@
+import "./TimeClock.css";
+
+
+ const TimeClock  = (props) => {
+    return (
+        <div className="Number mt-10">
+           <TimeClock></TimeClock>
+</div>
+)};
+
+           export default TimeClock;
+
